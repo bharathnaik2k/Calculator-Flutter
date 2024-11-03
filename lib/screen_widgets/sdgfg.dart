52ddfdcdf9fab527af6e6a642bbc99db51d8d900
Bharath Naik <bharathnaik868@gmail.com>
2024-11-03 11:45:06 +0530

    check

---
+++ lib/screen_widgets/sdgfg.dart
@@ -0,0 +1,27 @@
+
+class _HomeScreenState extends State<HomeScreen> {
+  // int
+  var input;
+  var output;
+
+  TextEditingController input1 = TextEditingController();
+  TextEditingController input2 = TextEditingController();
+
+  inputTeak(var value) {
+    if (input1.text.isEmpty) {
+      setState(() {
+        input1.text = value;
+      });
+    } else {}
+  }
+
+  // void equalPressed() {
+  //   String finaluserinput = userInput;
+  //   finaluserinput = userInput.replaceAll('x', '*');
+
+  //   Parser p = Parser();
+  //   Expression exp = p.parse(finaluserinput);
+  //   ContextModel cm = ContextModel();
+  //   double eval = exp.evaluate(EvaluationType.REAL, cm);
+  //   answer = eval.toString();
+  // }
